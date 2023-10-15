6bc7995d6ddee8b59b68acc4c0f29e773bd4f2f4
Bouchra37 <ennahhalbouchra@gmail.com>
2023-10-15 20:13:28 +0100

    first commit

---
+++ app/src/main/java/ma/projet/recycle/SplashActivity.java
@@ -0,0 +1,39 @@
+package ma.projet.recycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(SplashActivity.this, ListActivity.class);
+                    startActivity(intent);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        t.start();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.finish();
+    }
+}
